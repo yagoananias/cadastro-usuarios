68c1ec146cc29de2466a522ba995f9de62754644
Yago Ananias <yago_souza92@yahoo.com.br>
2020-07-06 19:26:50 -0300

    Creating Add User Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import AddUser from './components/AddUser'
 import EditUser from './components/EditUser'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <AddUser/>
-      <EditUser/>
+    <div style={{maxWidth: "10rem", margin: "2rem auto"}}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <AddUser path="/add" component={AddUser} />
+          <EditUser path="/edit/:id" component={EditUser} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const AddUser = () => {
   return (
-    <div>
-      <h1>AddUser</h1>
-    </div>
+    <Form>
+      <FormGroup>
+        <Label>Name</Label>
+        <Input type="text" placeholder="Enter Name"></Input>
+      </FormGroup>
+      <Button type="submit">Submit </Button>
+      <Link to="/" className="btn btn-danger ml-2" style={{}}>Cancel</Link>
+    </Form>
   )
 }
 
